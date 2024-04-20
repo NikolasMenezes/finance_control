b9f9050805c65e6cef80b40b61bec14323653e01
Nikolas Menezes <nikolasdev.contato@gmail.com>
2024-04-20 16:37:12 -0300

    fix: add hidden class to menu on lg

--- assets/js/utils/menu.js
+++ assets/js/utils/menu.js
@@ -1,10 +1,12 @@
 export function openMenu(menuElement) {
   menuElement.classList.add("right-0");
+  menuElement.classList.remove("hidden");
   menuElement.classList.remove("-right-1/2");
   document.body.classList.add("overflow-y-hidden");
 }
 
 export function closeMenu(menuElement) {
+  menuElement.classList.remove("hidden");
   menuElement.classList.add("-right-1/2");
   menuElement.classList.remove("right-0");
   document.body.classList.remove("overflow-y-hidden");
