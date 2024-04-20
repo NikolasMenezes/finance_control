f444980c86b73e6cd72e4bc6e1dc71b344d43dcd
Nikolas Menezes <nikolasdev.contato@gmail.com>
2024-04-20 18:37:20 -0300

    feat: create initial profile view ui

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,13 +1,17 @@
 import { logout } from "./utils/logout.js";
-import { defineTheme } from "./utils/theme.js";
+import { defineTheme, handleTheme } from "./utils/theme.js";
 import { closeMenu, openMenu } from "./utils/menu.js";
 
 const logoutTrigger = document.querySelectorAll(".logout-trigger");
 const openMenuBtn = document.querySelector("#menu-trigger");
 const closeMenuBtn = document.querySelector("#close-menu-trigger");
 const menuMobileContainer = document.querySelector("#menu-mobile");
+const themeSelect = document.querySelector("#theme-select");
 
 defineTheme();
 openMenuBtn.addEventListener("click", () => openMenu(menuMobileContainer));
 closeMenuBtn.addEventListener("click", () => closeMenu(menuMobileContainer));
 logoutTrigger.forEach((trigger) => trigger.addEventListener("click", logout));
+themeSelect.addEventListener("change", ({ target }) =>
+  handleTheme(target.options[target.selectedIndex].value)
+);
--- assets/js/utils/theme.js
+++ assets/js/utils/theme.js
@@ -1,10 +1,25 @@
 export function defineTheme() {
   const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const userPreferredTheme = localStorage.getItem("theme");
 
-  if (darkThemeMq) {
+  if (darkThemeMq || userPreferredTheme === "dark") {
     document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+}
+
+export function handleTheme(theme) {
+  if (theme === "system") {
+    localStorage.removeItem("theme");
+    defineTheme();
     return;
   }
-
-  document.body.classList.remove("dark");
+  if (theme === "dark") {
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  }
 }
